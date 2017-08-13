7920bd541507d35990f146aa45168a4b2f4331be
Ron Martinez <ramartin.net@gmail.com>
2017-08-13 18:11:08 -0400

    Include the missing `see` function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict"
 
-const examine = require("./lib/examine")
+const { examine, see } = require("./lib/examine")
 const inspect = require("./lib/inspect")
 const isPlainObject = require("./lib/is-plain-object")
 const whereAll = require("./lib/where-all")
 
-module.exports = { examine, inspect, isPlainObject, whereAll }
+module.exports = { examine, inspect, isPlainObject, see, whereAll }
