981486b7c3fcfcc0731608979097c4081f598d64
Ron Martinez <ramartin.net@gmail.com>
2017-08-15 00:29:24 -0400

    Fix missing `see` reference

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   examine: require("./lib/examine"),
   inspect: require("./lib/inspect"),
   isPlainObject: require("./lib/is-plain-object"),
-  see: require("./lib/examine"),
+  see: require("./lib/see"),
   uniquesFor: require("./lib/uniquesFor"),
   whereAll: require("./lib/where-all"),
 }
