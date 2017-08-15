069567844ed9d5c55d462e5ae80fdf7fe467590a
Ron Martinez <ramartin.net@gmail.com>
2017-08-15 00:09:50 -0400

    Fix missing `uniquesFor` reference

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = {
   inspect: require("./lib/inspect"),
   isPlainObject: require("./lib/is-plain-object"),
   see: require("./lib/examine"),
+  uniquesFor: require("./lib/uniquesFor"),
   whereAll: require("./lib/where-all"),
 }
