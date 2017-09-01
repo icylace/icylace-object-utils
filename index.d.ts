6a298e3264aeb145be2285311a735ccfa9d65d2c
Ron Martinez <ramartin.net@gmail.com>
2017-08-31 21:29:42 -0400

    Update type declaration for `uniquesFor`

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@ export function examine(spec: any, data: any, props: Object): Object | null
 export function inspect(spec: any, data: any): Object
 export function isPlainObject(it: any): boolean
 export function see(cond: (value: any) => boolean, prop: string, detecting: boolean | null): (value: any) => string | boolean | null
-export function uniquesFor(a: any[][]): any[]
+export function uniquesFor(...a: any[]): any[]
 export function whereAll(spec: any, data: any): boolean
