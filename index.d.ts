73b1cd92cb9895356f4ac93bd21d0cdca1c7b3e5
Ron Martinez <ramartin.net@gmail.com>
2017-08-31 21:21:19 -0400

    Add TypeScript declarations

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export function entriesAsObject(entries: [string | number, any][]): Object
+export function examine(spec: any, data: any, props: Object): Object | null
+export function inspect(spec: any, data: any): Object
+export function isPlainObject(it: any): boolean
+export function see(cond: (value: any) => boolean, prop: string, detecting: boolean | null): (value: any) => string | boolean | null
+export function uniquesFor(a: any[][]): any[]
+export function whereAll(spec: any, data: any): boolean
