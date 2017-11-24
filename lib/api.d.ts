61fa929f6a31509675a28fd05b0ded10bb22c064
Ron Martinez <ramartin.net@gmail.com>
2017-11-24 14:40:46 -0500

    build: Start using Prettier (https://prettier.io/)

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -3,7 +3,11 @@ declare namespace IcylaceObjectUtils {
   export function examine(spec: any, data: any, props?: Object): Object | null
   export function inspect(spec: any, data: any): Object
   export function isPlainObject(it: any): boolean
-  export function see(predicate: (value: any) => boolean, prop: string, detecting?: boolean | null): (value: any) => string | boolean | null
+  export function see(
+    predicate: (value: any) => boolean,
+    prop: string,
+    detecting?: boolean | null,
+  ): (value: any) => string | boolean | null
   export function uniquesFor(...a: any[]): any[]
   export function whereAll(spec: any, data: any): boolean
 }
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+"use strict"
+
+module.exports = {
+  bracketSpacing: true,
+  insertPragma: false,
+  printWidth: 120,
+  proseWrap: false,
+  rangeEnd: Infinity,
+  rangeStart: 0,
+  requirePragma: false,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+}
